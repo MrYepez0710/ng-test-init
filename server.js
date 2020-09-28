f2d64130f9533aecae48279671d0354f05242322
Manuel Yepez <jmyv.0710@gmail.com>
2020-09-28 12:33:59 -0500

    DEV: se hacen ajustes para heroku

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ app.use(express.static(__dirname+'/dist/blogyager'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/blogyager/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
